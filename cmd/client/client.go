5164b556fa9cac2850a5467d4d0c098d047b5c9f
azziz abelque <azziz@cit.io>
2020-05-30 16:48:04 +0200

    WIP prompt

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,28 +1,91 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"strings"
+
+	"github.com/chzyer/readline"
 )
 
 const (
-	Quit = "quit\n"
+	help     = "help"
+	status   = "status"
+	launch   = "launch"
+	relaunch = "relaunch"
+	reload   = "reload"
+	stop     = "stop"
+	quit     = "quit"
 )
 
+var completer = readline.NewPrefixCompleter(
+	readline.PcItem("status"),
+	readline.PcItem("launch"),
+	readline.PcItem("relaunch"),
+	readline.PcItem("reload"),
+	readline.PcItem("stop"),
+	readline.PcItem("quit"),
+)
+
+func help_message() {
+	var message strings.Builder
+	message.WriteString("default commands:\n")
+	message.WriteString("=================\n")
+	message.WriteString("help\tstatus\tlaunch\n")
+	message.WriteString("relaunch\treload\tstop\n")
+	message.WriteString("quit\n")
+
+	fmt.Println(message.String())
+}
+
 func main() {
-	// set_raw_mode()
-	// unset_raw_mode()
-	Reader := bufio.NewReader(os.Stdin)
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:            "taskmaster> ",
+		HistoryFile:       ".history_taskmaster",
+		InterruptPrompt:   "^C",
+		EOFPrompt:         "quit",
+		AutoComplete:      completer,
+		HistorySearchFold: true,
+	})
+	if err != nil {
+		os.Exit(1)
+	}
+	defer l.Close()
+
+	log.SetOutput(l.Stderr())
 	for {
-		fmt.Print("Taskmaster> ")
-		text, err := Reader.ReadString('\n')
-		fmt.Print(text)
-		if err != nil {
-			os.Exit(1)
+		line, err := l.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
+				continue
+			}
+		} else if err == io.EOF {
+			break
 		}
 		switch {
-		case text == Quit:
+		case line == help:
+			// request.go
+			help_message()
+		case line == status:
+			// request.go
+			log.Println("Status done!") // Ideas to inform user
+		case line == launch:
+			// request.go
+			log.Println("Launch done!") // Ideas to inform user
+		case line == relaunch:
+			// request.go
+			log.Println("Relaunch done!") // Ideas to inform user
+		case line == reload:
+			// request.go
+			log.Println("Reload done!") // Ideas to inform user
+		case line == stop:
+			// request.go
+			log.Println("Stop done!") // Ideas to inform user
+		case line == quit:
 			os.Exit(0)
 		}
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kyazdani42/taskmaster
 
 go 1.14
 
-require github.com/BurntSushi/toml v0.3.1
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e
+)
