50791266e763f8ebc864df2a71916ad86c98c913
azziz abelque <azziz@cit.io>
2020-05-30 22:17:41 +0200

    add TrimSpace to line before split

--- cmd/client/parser.go
+++ cmd/client/parser.go
@@ -28,20 +28,12 @@ func check_command(command string) {
 }
 
 func parser_line(line string) {
-	args := strings.Split(line, " ")
+	args := strings.Split(strings.TrimSpace(line), " ")
 	if len(args) == 1 {
 		check_command(strings.TrimSpace(args[0]))
 	} else {
 		command, process := args[0], args[1:]
-		if len(process) == 1 {
-			x := strings.TrimSpace(process[0])
-			if len(x) == 0 {
-				check_command(command)
-			}
-			//TODO request to server
-		} else {
-			//TODO request to server
-			fmt.Println(command, process)
-		}
+		//TODO request to server
+		fmt.Println(command, process)
 	}
 }
